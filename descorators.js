304bcce5ede988cca6bd56ef4ad8eb7b2076551a
Max Nordlund <max.nordlund@gmail.com>
2015-12-20 08:15:11 +0100

    Clean up implementation
    This removes a few of the caveats and weirdness that curry/memoize had
    before. It also adds a bunch of documentation to make it crystal clear
    what's going on.

--- descorators.js
+++ descorators.js
@@ -10,8 +10,8 @@ const { hasOwnProperty, getOwnPropertyNames, getOwnPropertyDescriptor, definePro
  *
  * Valid access options are in {@link descriptorAccessKeys}
  *
- * @param {Object.<string, boolean>|...string} options to set, either an object
- *                                                     or list of keys to enable
+ * @param {(Object.<string, boolean>|...string)} options either a list of keys
+ *        to enable or a map of options to set explicitly.
  */
 export function ability(options, ...rest) {
   if (typeof options === "string") {
@@ -20,7 +20,7 @@ export function ability(options, ...rest) {
   }
 
   return (target, name, descriptor) => {
-    descriptorAccessKeys.forEach((name) => descriptor[name] = options[name])
+    descriptorAccessKeys.forEach((name) => descriptor[name] = !!options[name])
   }
 }
 
@@ -39,8 +39,8 @@ const descriptorAccessKeys = Object.freeze([
  * @param {boolean} [configurability=true] to set, defaults to `true`
  */
 export function configurable(target, name, descriptor) {
-  if (typeof target === "boolean") return target::configurable
-  descriptor.configurable = (this instanceof Boolean) ? this: true
+  if (typeof target === "boolean") return configurable.bind(target)
+  descriptor.configurable = (this instanceof Boolean) ? this.valueOf(): true
 }
 
 /**
@@ -49,8 +49,8 @@ export function configurable(target, name, descriptor) {
  * @param {boolean} [enumerability=true] to set, defaults to `true`
  */
 export function enumerable(target, name, descriptor) {
-  if (typeof target === "boolean") return target::enumerable
-  descriptor.enumerable = (this instanceof Boolean) ? this: true
+  if (typeof target === "boolean") return enumerable.bind(target)
+  descriptor.enumerable = (this instanceof Boolean) ? this.valueOf(): true
 }
 
 /**
@@ -59,174 +59,292 @@ export function enumerable(target, name, descriptor) {
  * @param {boolean} [writability=true] to set, defaults to `true`
  */
 export function writable(target, name, descriptor) {
-  if (typeof target === "boolean") return target::writable
-  descriptor.writable = (this instanceof Boolean) ? this: true
+  if (typeof target === "boolean") return writable.bind(target)
+  descriptor.writable = (this instanceof Boolean) ? this.valueOf(): true
 }
 
+/**
+ * @callback use~target
+ * @param {...*} [parameters] for the targeted function
+ * @return {*} return value of the targeted function
+ */
+
+/**
+ * @callback use~advice
+ * @param {use~target} target to wrap with this advice
+ * @return {use~target} wrapper function which applies this advice to the target
+ */
+
+/**
+ * @callback use~accessor
+ * @param {use~target} getter to wrap with this advice
+ * @param {use~target} setter to wrap with this advice
+ * @return {{getter: use~target, setter: use~target}} wrapped accessor pair
+ */
+
+/**
+ * @typedef {Object} use~aspect
+ * @property {use~advice} class class specfic advice if value hasn't been set
+ * @property {use~advice} value decorator
+ * @property {use~accessor} accessor decorator for both the getter/setter part,
+ *           if this is proved the get/set properties are ignored
+ * @property {use~advice} get decorator for the getter part of an accessor
+ * @property {use~advice} set decorator for the setter part of an accessor
+ */
+
+/**
+ * Meta descriptor to (re)use simple wrapper function(s).
+ *
+ * @param {(use~aspect|use~advice)} wrapper either a single advice to apply too
+ *        all types of descriptors, or an aspect mapping each type to an advice.
+ *
+ * @example <caption>Using Bluebirds method wrapper.</caption>
+ * class MyClass {
+ *   @use(Promise.method)
+ *   willAlwaysReturnPromise() {
+ *     return true
+ *   }
+ * }
+ *
+ * @example <caption>A simple logger decorator.</caption>
+ * const logger = {
+ *   class(klass) {
+ *     console.log("Class", klass)
+ *     return klass
+ *   },
+ *
+ *   value(fn) {
+ *     console.log("Method", fn.name, "taking", fn.length, "parameters")
+ *     return fn
+ *   }
+ *
+ *   get(getter) {
+ *     console.log("Getter")
+ *     return getter
+ *   }
+ * }
+ *
+ * @use(logger)
+ * class MyClass {
+ *   @use(logger)
+ *   someMethod() {}
+ *
+ *   @use(logger)
+ *   otherMethod() {}
+ *
+ *   @use(logger)
+ *   get random() {
+ *     return 4 // Chosen by fair dice roll
+ *   }
+ * }
+ */
 export function use(aspect) {
+  if (typeof aspect === "function") {
+    /** Wrap the input {@link use~advice} into an {@link use~aspect} */
+    aspect = {
+      class: aspect,
+      value: aspect,
+      get: aspect,
+      set: aspect
+    }
+  }
+
   return (target, name, descriptor) => {
     if (typeof target === "function" && name == null && descriptor == null) {
-      return aspect(target)
-    } else if (descriptor::hasOwnProperty("value")) {
-      descriptor.value = aspect(descriptor.value)
+      // Class decorator
+      return aspect.class(target)
+    } else if (descriptor.hasOwnProperty("value")) {
+      // Method/value decorator
+      descriptor.value = aspect.value(descriptor.value)
     } else {
-      const { get: getter, set: setter } = descriptor
-      descriptor.get = descriptor.set = aspect(function accessor(value) {
-        if (arguments.length === 0) {
-          return this::getter()
-        } else {
-          return this::setter(...arguments)
-        }
-      })
+      // Accessor decorator
+      if (aspect.accessor) {
+        const { get, set } = aspect.accessor(descriptor.get, descriptor.set)
+        descriptor.get = get
+        descriptor.set = set
+      } else {
+        if (aspect.get) descriptor.get = aspect.get(descriptor.get)
+        if (aspect.set) descriptor.set = aspect.set(descriptor.get)
+      }
     }
   }
 }
 
-export function curry(target, name, descriptor) {
-  if (descriptor && descriptor::hasOwnProperty("value") && typeof descriptor.value !== "function") {
-    throw new TypeError(`Can not curry non-function values`)
-  } else if (descriptor && (descriptor::hasOwnProperty("get") || descriptor::hasOwnProperty("set"))) {
-    throw new TypeError(`Can not curry accessors`)
+/**
+ * @callback use~accessor jQuery style property accessor function
+ * @param {*} [value] sets the value if provided, just gets it otherwise
+ * @return {*} current value for this property
+ */
+
+/**
+ * Turn a jQuery style property accessor method into a proper ES2015 accessor.
+ *
+ * @param {accessor~method} method to decorate
+ */
+export function accessor(target, name, descriptor) {
+  if (descriptor && descriptor.hasOwnProperty("value") && typeof descriptor.value !== "function") {
+    throw new TypeError(`${name} is not a function`)
+  } else if (descriptor && (descriptor.hasOwnProperty("get") || descriptor.hasOwnProperty("set"))) {
+    throw new TypeError(`${name} is already an accessors`)
+  } else if (typeof target === "function" && name == null && descriptor == null) {
+    throw new TypeError(`Can not decorate class ${name}`)
   }
 
-  if (typeof target === "function" && name == null && descriptor == null) {
-    // Class decorator
-    return function curriedClass() {
-      if (this instanceof curriedClass) {
-        throw new TypeError(`Curried constructor ${target.name} prohibits use of 'new'`)
-      }
+  descriptor.get = descriptor.set = descriptor.value
+  delete descriptor.value
+}
 
+/**
+ * Enables currying the decorated method/class.
+ *
+ * Works by checking the parameter length of the supplied function, and as such
+ * it won't work with `arguments` shenanigans. If you're using a compile to JS
+ * language make sure to check the compiled output.
+ */
+export const curry = use({
+  accessor(_getter, _setter) { throw new TypeError(`Can not curry accessors`) },
+  value(target) {
+    if (typeof target !== "function") {
+      throw new TypeError(`Can not curry non-function values`)
+    }
+
+    return function curried() {
       if (arguments.length < target.length) {
-        return bind.bind(curriedClass, null)(...arguments)
+        return bind.bind(curried, this).apply(void 0, arguments)
       } else {
-        return new (bind.bind(target, null)(...arguments))
-      }
-    }
-  } else {
-    // Method decorator
-    const method = descriptor.value
-    descriptor.value = function curriedMethod() {
-      if (arguments.length < method.length) {
-        return bind.bind(curriedMethod, this)(...arguments)
-      } else {
-        return this::method(...arguments)
+        return target.apply(this, arguments)
       }
     }
   }
-}
+})
 
-export function memoize(target, name, descriptor) {
-  if (descriptor && descriptor::hasOwnProperty("value") && typeof descriptor.value !== "function") {
-    throw new TypeError(`Can not memoize non-function values`)
-  }
+/**
+ * Memoize the decorated method/class.
+ *
+ * That is, for a given ordered set of parameters this will cache the results
+ * and when the same parameters are used it wil serve the result from cache.
+ *
+ * This handles references as well, and uses WeakMaps behind the scenes to make
+ * sure those references can be garbage collected. However this also means it
+ * uses reference equality for parameter checking.
+ *
+ * Currently it has an unbound cache, save for the use of WeakMaps to scope the
+ * cache to the `this` value of the method. This combined means that as the
+ * instances of a class get garbage collected, so those their cache.
+ */
+export const memoize = use({
+  class: function(target) {
+    const cache = new Cache(target)
 
-  if (typeof target === "function" && name == null && descriptor == null) {
-    // Class decorator
-    const store = new Map(),
-          instances = new WeakMap(),
-          bound = bind.bind(target, null)
-
-    function init() {
-      const curried = bound(...arguments)
-      return () => new curried()
-    }
-
-    class MemoizedClass extends target {
-      constructor() {
-        instances.set(this, { store, init }::retrieveByArguments(...arguments))
-        proxyProperties(instances, this, this)
+    return class MemoizedClass extends target {
+      constructor(...parameters) {
+        // Since `this` will always point to a new value, use `target` as context
+        return cache.fetch(target, parameters, () => new target(...parameters))
       }
     }
+  },
+  value(target) {
+    const cache = new Cashe(target)
 
-    proxyProperties(instances, target.prototype, MemoizedClass.prototype)
-
-    return MemoizedClass
-  } else if (typeof descriptor.value === "function") {
-    // Method decorator
-    const cache = new WeakMap(),
-          bound = new WeakMap(),
-          method = descriptor.value
-
-    descriptor.value = function memoizedMethod() {
-      const init = retrieve(bound, this, () => bind.bind(method, this)),
-            store = retrieve(cache, this, () => new Map())
-
-      return { store, init }::retrieveByArguments(...arguments)
-    }
-  } else {
-    // Accessor (getter/setter)
-    const cache = new WeakMap(),
-          { get: getter, set: setter } = descriptor
-
-    descriptor.get = function memoizedGetter() {
-      return retrieve(cache, this, this::getter)
+    function memoized(...parameters) {
+      return cache.fetch(this, parameters, () => target.apply(this, parameters))
     }
 
-    descriptor.set = function memoizedSetter(value) {
-      if (typeof setter === "function") {
-        cache.set(this, this::setter(value))
-      } else if (value === void 0) {
-        cache.delete(this)
-      } else {
-        throw new TypeError(`Default memoized setter only accepts 'undefined'`)
+    Object.setPrototypeOf(memoized, target)
+    memoized.prototype = Object.create(target.prototype)
+  },
+  accessor(getter, setter) {
+    let cache, invalid = true
+
+    return {
+      get() {
+        if (invalid) {
+          cache = getter.call(this)
+          invalid = false
+        }
+        return cache
+      },
+      set(value) {
+        cache = void 0 // Clear the cache to avoid memory leaks
+        invalid = true
+        return setter.call(this, value)
       }
-      return cache.get(this)
     }
   }
+})
+
+/**
+ * A Map wrapper with a default value generator, a bit like in python/ruby.
+ *
+ * @private
+ * @class
+ */
+class DefaultMap {
+  constructor(store, fallback) {
+    this.store = store
+    this.fallback = fallback
+  }
+
+  get(key) {
+    return this.fetch(key, this.fallback)
+  }
+
+  fetch(key, fallback) {
+    if (!this.store.has(key)) this.store.set(key, fallback())
+    return this.store.get(key)
+  }
 }
 
-// Private helpers
+/**
+ * A cache for a method/class
+ *
+ * This is very the bulk of the work for {@link memoize} is done.
+ */
+class Cache {
+  /** The global unique identification counter */
+  static guuid = 1
 
-let guuid = 1
-const references = new WeakMap()
+  /** This holds the weak references to all parameters any memozied function uses */
+  static references = new DefaultMap(new WeakMap(), () => Cache.guuid++)
 
-function retrieve(store, key, create) {
-  store.has(key) || store.set(key, create())
-  return store.get(key)
-}
-
-function retrieveByArguments() {
-  return retrieve(this.store, keyFor(...arguments), this.init(...arguments))
-}
-
-function keyFor(...args) {
-  return args.map((it) => {
-    switch (typeof it) {
-      case "object":
-        if (it === null) return "null"
-      case "function":
-        return `<${retrieve(references, it, () => guuid++)}>`
-      case "string":
-        return `"${it.replace('"', '\"')}"`
-      default:
-        return `${it}`
-    }
-  }).join(",")
-}
-
-function proxyProperties(instances, src, dst) {
-  getOwnPropertyNames(src).forEach((name) => {
-    const descriptor = getOwnPropertyDescriptor(src, name)
-    let { get: getter, set: setter, value, writable } = descriptor
-
-    if (descriptor::hasOwnProperty("value")) {
-      delete descriptor.value
-      delete descriptor.writable
-
-      getter = function valueGetter() { return this[name] }
-      if (writable) {
-        setter = function valueSetter(value) { return this[name] = value }
+  /**
+   * Turns the provided parameters list into a string, essentially hashing it.
+   *
+   * @param {Array} parameters to hash
+   * @return {string} representing the parameters provided
+   */
+  static keyFrom(parameters) {
+    return parameters.map((it) => {
+      if (it == null) return `${it}` // `null` and `undefined`
+      switch (typeof it) {
+        case "object":
+        case "function": // Reference types
+          return `<${references.get(it)}>`
+        case "string": // Quote strings to prevent collisions with other types
+          return `"${it.replace('"', '\"')}"`
+        default: // Primitive types
+          return `${it}`
       }
-    }
+    }).join(",")
+  }
 
-    if (typeof getter === "function") {
-      descriptor.get = function get() { return instances.get(this)::getter() }
-    }
+  constructor(target) {
+    this.target = target
+    this.store = new DefaultMap(new WeakMap(), () => new DefaultMap(new Map(), null))
+  }
 
-    if (typeof setter === "function") {
-      descriptor.set = function set(value) { return instances.get(this)::setter(value) }
-    }
-
-    defineProperty(dst, name, descriptor)
-  })
+  /**
+   * Gets a result from this cache using the provided context and parameters.
+   *
+   * If there is no result, it uses the provided fallback to fetch the result.
+   * This is store in this cache for future reference.
+   *
+   * @param {Object} context for the fallback, aka `this`
+   * @param {Array} parameters for the fallback
+   * @param {Function} fallback for when the cache is empty, called with the above
+   * @return {*} result from the fallback, or a cached ditto
+   */
+  fetch(context, parameters, fallback) {
+    return this.store.get(context).fetch(Cache.keyFrom(parameters), fallback)
+  }
 }
